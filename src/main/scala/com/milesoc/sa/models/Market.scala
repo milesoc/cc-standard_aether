2112fa1a443743435a3f90453a634efcbf9699f5
Miles O'Connell <miles@stackmob.com>
2013-02-10 14:21:54 -0800

    Fix

--- src/main/scala/com/milesoc/sa/models/Market.scala
+++ src/main/scala/com/milesoc/sa/models/Market.scala
@@ -59,11 +59,11 @@ object Market {
     val markets = marketsRaw.map (marketRaw => {
       for {
         name <- Option(Reader.convertCommodityName(marketRaw))
-        _ <- logger.debug("found name %s".format(name))
+        _ <- Option(logger.debug("found name %s".format(name)))
         price <- Option(Reader.convertPrice(marketRaw))
-        _ <- logger.debug("found price %s".format(price))
+        _ <- Option(logger.debug("found price %s".format(price)))
         history <- convertPriceHistory(marketRaw, logger, name)
-        _ <- logger.debug("found history %s".format(history))
+        _ <- Option(logger.debug("found history %s".format(history)))
         market <- Some(new Market(name, price, history))
       } yield market
     })
