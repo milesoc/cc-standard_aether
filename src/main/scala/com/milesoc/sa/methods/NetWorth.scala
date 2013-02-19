2445d206822ed71a65f4a30622ea63bc52bed218
Miles O'Connell <miles@stackmob.com>
2013-02-18 22:05:48 -0800

    Net Worth

--- src/main/scala/com/milesoc/sa/methods/NetWorth.scala
+++ src/main/scala/com/milesoc/sa/methods/NetWorth.scala
@@ -1,10 +1,10 @@
 package com.milesoc.sa.methods
 
-
 import com.stackmob.core.customcode.CustomCodeMethod
 import com.stackmob.sdkapi._
 import com.stackmob.core.rest.{ResponseToProcess, ProcessedAPIRequest}
 import java.{lang, util}
+import lang.Long
 import util.{List => JList, Map => JMap}
 import scala.collection.JavaConverters._
 import com.milesoc.sa.core.Utils._
@@ -16,11 +16,11 @@ import com.stackmob.core.MethodVerb
  * User: miles
  * Date: 2/18/13
  */
-class CharacterMarkets extends CustomCodeMethod {
+class NetWorth extends CustomCodeMethod {
 
-  override def getMethodName: String = "character_markets"
+  override def getMethodName: String = "net_worth"
 
-  override def getParams: JList[String] = List[String]("id").asJava
+  override def getParams: JList[String] = List[String]().asJava
 
   override def execute(request: ProcessedAPIRequest, serviceProvider: SDKServiceProvider): ResponseToProcess = {
     try {
@@ -28,38 +28,28 @@ class CharacterMarkets extends CustomCodeMethod {
         case null => errorResponse(401, "Not logged in")
         case user => request.getVerb match {
           //for now user won't matter but in the future we can get only the user's characters
-          case MethodVerb.GET => getCharacterMarkets(request, serviceProvider, user)
+          case MethodVerb.GET => getCharacters(request, serviceProvider, user)
           case _ => errorResponse(400, "Only GET is allowed at the moment")
         }
       }
     } catch {
       case t => {
         val logger = serviceProvider.getLoggerService(getClass)
-        logger.error("Failed to execute character_markets", t)
+        logger.error("Failed to execute net worth!!!!!", t)
         errorResponse(500, "internal error")
       }
     }
   }
 
-  def getCharacterMarkets(request: ProcessedAPIRequest, provider: SDKServiceProvider, user: String): ResponseToProcess = {
-    val id = request.getParams.get("id")
-
-    val character = PlayerCharacter.getCharacter(id, provider)
+  def getCharacters(request: ProcessedAPIRequest, provider: SDKServiceProvider, user: String): ResponseToProcess = {
+    val characters = PlayerCharacter.getAllCharacters(provider)
     val resultMap = new util.HashMap[String, Object]()
-    val resultList = new util.ArrayList[JMap[String, Object]]()
-    character.markets foreach(entry => {
-      val marketMap = new util.LinkedHashMap[String, Object]()
-      marketMap.put("id", entry._2._1)
-      marketMap.put("market", entry._1.commodity)
-      marketMap.put("quantity", new lang.Long(entry._2._2))
-      marketMap.put("invested", new lang.Long(entry._2._2*entry._1.price))
-      resultList.add(marketMap)
-    })
+    val worth = characters.foldLeft(0L){(worth, pc) => {
+      worth + pc.money + pc.invested
+    }}
 
-    resultMap.put("result", resultList)
+    resultMap.put("worth", new Long(worth))
     new ResponseToProcess(200, resultMap)
   }
-
-
 
 }
